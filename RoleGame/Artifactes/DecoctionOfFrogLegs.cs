73951c7e5be346231f4496563e1f9c4aaceb4137
Артём Лукша <kiritorang11999@mail.ru>
2023-02-13 20:08:46 +0300

    13.02 20:08

---
+++ RoleGame/Artifactes/DecoctionOfFrogLegs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoleGame.Artifactes
+{
+    public class DecoctionOfFrogLegs : Artifact
+    {
+        public DecoctionOfFrogLegs(Character character) : base(0, false, character) { }
+        public override void Wiz(ref Character character, int force = 10)
+        {
+            Console.WriteLine("This artifact can't be used with force");
+            return;
+        }
+        public override void Wiz(ref Character character)
+        {
+            if(character.State != CharacterState.Poisoned)
+            {
+                Console.WriteLine($"The {character.Name} hasn't been poisoned!");
+                return;
+            }
+            else
+            {
+                if (character.CurrentHealth < character.MaxHealth * 0.1)
+                    character.State = CharacterState.Weakened;
+                else
+                    character.State = CharacterState.Normal;
+                Console.WriteLine($"The {character.Name} is {character.State.ToString()}");
+            }
+        }
+    }
+}
---
+++ RoleGame/Artifactes/PoisonousSaliva.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoleGame.Artifactes
+{
+    public class PoisonousSaliva : Artifact
+    {
+        public override void Wiz(ref Character character, int force = 10)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Wiz(ref Character character)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
